d6a804408fe5a240efac5dc506eb497fc6c8e147
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-11-30 13:21:08 +0100

    Ajouter le script saisir qui permet de saisir les informations de l'etudiant qui sont valie

---
+++ script/saisir.py
@@ -0,0 +1,43 @@
+"""
+Ce script valide les inputs de l'utilisateur en fonction du type de valeur attendue.
+"""
+
+
+def valider_input(message: str, f_validation, message_erreur):
+    while True:
+        valeur = input(message).strip()
+        if f_validation(valeur):
+            return valeur
+        print(message_erreur)
+
+
+def nom() -> str:
+    return valider_input(
+        "Saisie le nom de l'étudiant: ",
+        lambda x: x.isalpha(),
+        "Le nom n'est pas valide."
+    )
+
+
+def prénom() -> str:
+    return valider_input(
+        "Saisie le prénom de l'étudiant: ",
+        lambda x: x.isalpha(),
+        "Le prénom n'est pas valide."
+    )
+
+
+def age() -> int:
+    return int(valider_input(
+        "Saisie l'age de l'étudiant: ",
+        lambda x: x.isdigit() and  0 < int(x) < 100,
+        "L'age n'est pas valide."
+    ))
+
+
+def moyenne() -> float:
+    return round(float(valider_input(
+        "Saisie la moyenne de l'étudiant: ",
+        lambda x: x.isdecimal() and  0 <= float(x) <= 20,
+        "La moyenne n'est pas valide."
+    )), 2)
