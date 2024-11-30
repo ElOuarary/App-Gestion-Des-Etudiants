d95cf4a8ceb3b8c311c319abf0a0844f7a146918
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-11-30 15:52:03 +0100

    Ajouter des fonctions de filtrage par rapport a l'une des informations des etudiants dans le script operations.

--- script/operation.py
+++ script/operation.py
@@ -13,7 +13,19 @@ def ajouter_etudiants(df: pd.DataFrame) -> None:
         df.loc[len(df)] = [nom, prénom, age, moyenne]
 
 
-def chercher_etudiant(df: pd.DataFrame):
+def chercher_etudiant(df: pd.DataFrame) -> pd.DataFrame:
     nom: str = saisir.nom()
     prénom: str = saisir.prénom()
     return df[(df["Nom"]==nom) & (df["Prénom"]==prénom)]
+
+
+def filtrer_nom(df: pd.DataFrame, nom: str) -> pd.DataFrame:
+    return df[df["Nom"] == nom]
+
+
+def filtrer_age(df: pd.DataFrame, min_age: int, max_age: int = 99) -> pd.DataFrame:
+    return df[max_age >= df["Age"] >= min_age]
+
+
+def filtrer_moyenne(df: pd.DataFrame, min_moyenne: float, max_moyenne: float) -> pd.DataFrame:
+    return df[max_moyenne >= df["Moyenne"] >= min_moyenne]
