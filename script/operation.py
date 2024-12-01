008d4c7f41ef62798c9a7783b86c3d9854a53f5d
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-12-01 18:30:53 +0100

    Ameliorer les fonctions dans le script operation.py

--- script/operation.py
+++ script/operation.py
@@ -7,24 +7,28 @@ import saisir
 def ajouter_etudiants(df: pd.DataFrame, path: str) -> None:
     while True:
         étudiant = Etudiant()
-        nom: str = étudiant.nom
-        prénom: str =  étudiant.prénom
-        age: int = étudiant.age
-        moyenne: float = étudiant.moyenne
-        nouveau_étudiant = pd.Series({"Nom": nom, "Prénom": prénom, "Age": age, "Moyenne": moyenne})
-        if (df.apply(lambda row: row.equals(nouveau_étudiant), axis=1)).any():
-            print(f"Etudiant {nom} {prénom} existe déjà.")
-            continue
-        df = pd.concat([df, nouveau_étudiant.to_frame().T], ignore_index=False)
-        print(f"Etudiant {nom} {prénom} ajouté avec succés.")
 
-        print(f"Si tu veux ajouter un autre étudiant clique sur 'entrée', sinon clique sur n'import quelle touche")
-        while True:
-            if keyboard.is_pressed("enter"):
-                break
-            elif not keyboard.is_pressed("enter"):
-                df.to_csv(path)
-                return
+        nouveau_étudiant = pd.Series({
+            "Nom": étudiant.nom,
+            "Prénom": étudiant.prénom,
+            "Age": étudiant.age,
+            "Moyenne": étudiant.moyenne})
+        
+        if not df[(df["Nom"] ==  étudiant.nom) & (df["Prénom"] == étudiant.prénom)].empty:
+            print(f"Etudiant {étudiant.nom} {étudiant.prénom} existe déjà.")
+            continue
+
+        df = pd.concat([df, nouveau_étudiant.to_frame().T], ignore_index=True)
+        print(f"Etudiant {étudiant.nom} {étudiant.prénom} a été ajouté avec succès.")
+
+        continuer = input("Appuyez sur 'Entrée' pour continuer ou une autre touche pour quitter: ")
+        if continuer.strip() != "":
+            try:
+                df.to_csv(path, index=False)
+                print("Données sauvegardées avec succès.")
+            except Exception as e:
+                print(f"Erreur lors de la sauvegarde des données: {e}")
+            return
 
 
 def chercher_etudiant(df: pd.DataFrame) -> pd.DataFrame:
@@ -41,12 +45,15 @@ def filtrer_nom(df: pd.DataFrame, nom: str) -> pd.DataFrame:
 
 
 def filtrer_age(df: pd.DataFrame, min_age: int, max_age: int = 99) -> pd.DataFrame:
-    return df[max_age >= df["Age"] >= min_age]
+    return df[(max_age >= df["Age"]) & (df["Age"] >= min_age)]
 
 
 def filtrer_moyenne(df: pd.DataFrame, min_moyenne: float, max_moyenne: float = 20) -> pd.DataFrame:
-    return df[max_moyenne >= df["Moyenne"] >= min_moyenne]
+    return df[(max_moyenne >= df["Moyenne"]) & (df["Moyenne"]>= min_moyenne)]
 
 
 def calculer_moyenne_generale(df: pd.DataFrame) -> float:
+    if df.empty:
+        print("Aucune donnée disponible pour calculer la moyenne.")
+        return 0.0
     return df["Moyenne"].mean()
