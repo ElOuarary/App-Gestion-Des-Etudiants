efcd148723aadb51912abd68d31a882a7ad23738
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-12-09 03:21:16 +0800

    Optimizer le script saisir.py afin de manager plus de complexité.

--- script/saisir.py
+++ script/saisir.py
@@ -3,53 +3,46 @@ Ce script valide les inputs de l'utilisateur en fonction du type de valeur atten
 """
 from time import sleep
 
-def valider_input(message: str, f_validation, message_erreur):
+def valider_input(message: str, f_validation, message_erreur, convert_type=None):
     while True:
         valeur = input(message).strip()
         sleep(0.5)
         if f_validation(valeur):
-            return valeur
+            return convert_type(valeur) if convert_type else valeur
         print(message_erreur)
 
 
 def nom() -> str:
-    return valider_input(
+    return " ".join(valider_input(
         "Saisie le nom de l'étudiant: ",
         lambda x: x.replace(" ", "").isalpha(),
         "Le nom n'est pas valide."
-    ).title()
+    ).split()).title()
 
 
 def prénom() -> str:
-    return valider_input(
+    return " ".join(valider_input(
         "Saisie le prénom de l'étudiant: ",
         lambda x: x.replace(" ", "").isalpha(),
         "Le prénom n'est pas valide."
-    ).title()
+    ).split()).title()
 
 
-def age() -> int:
-    while True:
-        try:
-            return int(valider_input(
-                "Saisie l'age de l'étudiant: ",
-                lambda x: x.isdigit() and  0 < int(x) < 100,
-                "L'age n'est pas valide."
-            ))
-        except ValueError:
-            print("Veuillez entrer un nombre entier valide.")
+def age() -> int:    
+    return valider_input(
+        "Saisie l'age de l'étudiant: ",
+        lambda x: x.isdigit() and  0 < int(x) < 100,
+        "L'age n'est pas valide.",
+        int
+        )
 
 
 def moyenne() -> float:
-    while True:
-        try:
-            return round(float(
-                        valider_input(
-                            "Saisie la moyenne de l'étudiant: ",
-                            lambda x: x.replace(".", "", 1).isdigit() and  0 <= float(x) <= 20,
-                            "La moyenne n'est pas valide."
-                        )
-                        ),
-                    2)
-        except ValueError:
-            print("Veuillez entrer un nombre decimal valide.")
+    return round(
+                valider_input(
+                    "Saisie la moyenne de l'étudiant: ",
+                    lambda x: x.replace(".", "", 1).isdigit() and  0 <= float(x) <= 20,
+                    "La moyenne n'est pas valide.",
+                    float
+                ),
+                2)
