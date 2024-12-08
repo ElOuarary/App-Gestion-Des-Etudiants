a71e0e39310a78692ba273bca08371bcd4bb0f4c
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-12-08 21:43:20 +0100

    Optimizer le script main.py

--- script/main.py
+++ script/main.py
@@ -10,98 +10,94 @@ def menu_option(*args) -> None:
     return "\n".join(args)
 
 
-def interface(message: str, durée: int, fonction):
+def interface(message: str, durée: int):
     print(message)
     sleep(durée)
-    print(fonction)
 
 
 def option1(path: str) -> str:
     if os.path.exists(path):
         print(f"Le tableau existe déjà.")
-        main()
+        return
     # Initialiser le dataFrame où les informations des étudiants vont être stocké
     columns: list[str] = ["Nom", "Prénom", "Age", "Moyenne"]
     df = pd.DataFrame(columns=columns)
     df.to_csv(path, index=False)
     print(f"Le Tableau a été crée.\n")
-    main()
 
 
 def option2(path: str) -> str:
-    print(menu_option(
-        "1. Tableau",
-        "2. Etudiant",
-        "3. Moyenne",
-        "4. Retour"
-    ))
+    if not os.path.exists(path):
+        print(f"Vous devez initialiser un tableau.\n")
+        return
+    
     while True:
+        print(menu_option(
+            "1. Tableau",
+            "2. Etudiant",
+            "3. Moyenne",
+            "4. Retour"
+        ))
+
         choix: str = valider_input(
             "Choisir une option: ",
             lambda x: x.isdigit() and int(x) in range(1, 5),
             "Option non validé."
         )
         
-        if not os.path.exists(path):
-            print(f"Vous devez initialiser un tableau.\n")
-            main()
         df: pd.DataFrame =  pd.read_csv(path)
         match choix:
             case "1":
-                interface("Affichage du tableau...", 1.5, df)
+                interface("Affichage du tableau...", 1.5)
+                print(df)
             case "2": 
                 operation.chercher_etudiant(df)
             case "3":
                 operation.chercher_note(df)
             case "4":
-                pass
-        print()
-        main()
+                return
 
 
-def option3(path: str) -> str:
+def option3(path: str) -> None:
     if not os.path.exists(path):
-        return f"Vous devez initialiser un tableau.\n"
+        print(f"Vous devez initialiser un tableau.\n")
+        return
     df: pd.DataFrame = pd.read_csv(path)
     operation.ajouter_etudiants(df, path)
-    print()
-    main()
 
 
 def option4(path: str) -> str:
     if not os.path.exists(path):
         print(f"Vous devez initialiser un tableau.\n")
-        main()
+        return
     df: pd.DataFrame = pd.read_csv(path)
     operation.chercher_etudiant(df)
-    print()
-    main()
 
 
 def option5(path: str) -> None:
     if not os.path.exists(path):
-        return f"Vous devez initialiser un tableau.\n"
+        print(f"Vous devez initialiser un tableau.\n")
+        return
     df: pd.DataFrame = pd.read_csv(path)
     operation.calculer_moyenne_génerale(df)
-    print()
-    main()
 
 
 def option6(path: str) -> str:
-    print(menu_option(
-        "1. Tableau",
-        "2. Etudiant",
-        "3. Retour"
-    ))
+    if not os.path.exists(path):
+            print("Vous devez initialiser un tableau.\n")
+            return
+    
     while True:
+        print(menu_option(
+            "1. Tableau",
+            "2. Etudiant",
+            "3. Retour"
+        ))
         choix: str = valider_input(
             "Choisir une option: ",
             lambda x: x.isdigit() and int(x) in range(1, 4),
             "Option non validé."
         )
-        if not os.path.exists(path):
-            print("Vous devez initialiser un tableau.\n")
-            main()
         df: pd.DataFrame = pd.read_csv(path)
         match choix:
             case "1":
@@ -110,26 +106,25 @@ def option6(path: str) -> str:
             case "2":
                 print(operation.supprimer_étudiants(df))
             case "3":
-                pass
-        print()
-        main()
+                return
 
 
 def main() -> None:
-    print(
-        menu_option(
-            "Menu\n",
-            "1. Initialiser Tableau",
-            "2. Afficher",
-            "3. Ajouter Etudiant",
-            "4. Rechercher",
-            "5. Calculer Moyenne Génerale",
-            "6. Supprimer",
-            "7. Quitter"
-                    )
-        )
- 
+    path: str = "data/étudiants.csv" 
     while True:
+        print(
+            menu_option(
+                "Menu\n",
+                "1. Initialiser Tableau",
+                "2. Afficher",
+                "3. Ajouter Etudiant",
+                "4. Rechercher",
+                "5. Calculer Moyenne Génerale",
+                "6. Supprimer",
+                "7. Quitter"
+            )
+        )
+
         choix: str = valider_input(
             "Choisir une option: ",
             lambda x: x.isdigit() and int(x) in range(1, 8),
@@ -138,17 +133,19 @@ def main() -> None:
     
         match choix:
             case "1":
-                interface("Création du tableau...", 1.5, option1("data/étudiants.csv"))
+                interface("Création du tableau...", 1.5)
+                option1(path)
             case "2":
-                option2("data/étudiants.csv")
+                option2(path)
             case "3":
-                print(option3("data/étudiants.csv"))
+                print(option3(path))
             case "4":
-                print(option4("data/étudiants.csv"))
+                print(option4(path))
             case "5":
-                interface("Calcule de moyenne génerale des étudiants...", 1, option5("data/étudiants.csv"))
+                interface("Calcule de moyenne génerale des étudiants...", 1)
+                option5(path)
             case "6":
-                option6("data/étudiants.csv")
+                option6(path)
             case "7":
                 print("Fermeture d'application...")
                 sleep(1.5)
